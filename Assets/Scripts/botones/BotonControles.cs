f7772cfa98cbff44b1c781007db14b756ddd1ad6
Ro0o888 <al447517@uji.es>
2026-05-11 16:37:26 +0200

    ahora si esta

--- Assets/Scripts/botones/BotonControles.cs
+++ Assets/Scripts/botones/BotonControles.cs
@@ -4,6 +4,9 @@ public class BotonControles : MonoBehaviour
 {
     public GameObject cartelControl;
     public GameObject botonControl;
+    public GameObject botonPlay;
+    public GameObject botonExit;
+    public GameObject botonControles;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
@@ -11,6 +14,10 @@ public class BotonControles : MonoBehaviour
     {
         cartelControl.SetActive(true);
         botonControl.SetActive(true);
+        botonPlay.SetActive(false);
+        botonExit.SetActive(false);
+        botonControles.SetActive(false);
+
     }
     void Start()
     {
--- Assets/Scripts/botones/BotonSalir.cs
+++ Assets/Scripts/botones/BotonSalir.cs
@@ -4,12 +4,18 @@ public class BotonSalir : MonoBehaviour
 {
     public GameObject cartelControl;
     public GameObject botonControl;
+    public GameObject botonPlay;
+    public GameObject botonExit;
+    public GameObject botonControles;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public void cerrarVentana()
     {
         cartelControl.SetActive(false);
         botonControl.SetActive(false);
+        botonPlay.SetActive(true);
+        botonExit.SetActive(true);
+        botonControles.SetActive(true);
     }
     void Start()
     {
